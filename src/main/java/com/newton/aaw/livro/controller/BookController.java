236d55f0a6a9ddef2b2b6f3096767c1d199d7b8e
unknown <thallesgomesmello@gmail.com>
2021-10-04 22:33:39 -0300

    Feita correção na colletion delete

--- src/main/java/com/newton/aaw/livro/controller/BookController.java
+++ src/main/java/com/newton/aaw/livro/controller/BookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,6 +61,7 @@ public class BookController {
 		return new BookDto(book);
 	}
 	
+	@DeleteMapping("/books/{id}")
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		bookService.delete(id);
 		
